0678617c51fc48fc42fe108098f970003d5efa43
Christophe Coevoet <stof@notk.org>
2021-09-08 13:34:43 +0200

    Rewrite the tests to avoid mocking the event dispatcher
    This allows ensuring that we actually use it properly (and to get its
    deprecation warnings).

--- tests/MailerTest.php
+++ tests/MailerTest.php
@@ -2,9 +2,14 @@
 
 namespace Stampie\Extra\Tests;
 
+use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
+use Stampie\Extra\Event\MessageEvent;
 use Stampie\Extra\Mailer;
 use Stampie\Extra\StampieEvents;
+use Stampie\MailerInterface;
+use Stampie\MessageInterface;
+use Symfony\Component\EventDispatcher\EventDispatcher;
 
 class MailerTest extends TestCase
 {
@@ -14,34 +19,54 @@ class MailerTest extends TestCase
     private $mailer;
 
     /**
-     * @var \PHPUnit_Framework_MockObject_MockObject
+     * @var MockObject&MailerInterface
      */
     private $delegate;
 
     /**
-     * @var \PHPUnit_Framework_MockObject_MockObject
+     * @var EventDispatcher
      */
     private $dispatcher;
 
-    public function setUp()
+    protected function setUp(): void
     {
-        $this->delegate = $this->getMockBuilder('Stampie\MailerInterface')->getMock();
-        $this->dispatcher = $this->getMockBuilder('Symfony\Component\EventDispatcher\EventDispatcherInterface')->getMock();
+        $this->delegate = $this->getMockBuilder(MailerInterface::class)->getMock();
+        $this->dispatcher = new EventDispatcher();
         $this->mailer = new Mailer($this->delegate, $this->dispatcher);
     }
 
-    public function testSend()
+    public function testTriggersAnEventWithTheMessage()
     {
-        $message = $this->getMockBuilder('Stampie\MessageInterface')->getMock();
+        $message = $this->getMockBuilder(MessageInterface::class)->getMock();
+
+        $called = false;
+        $listener = function (MessageEvent $event) use ($message, &$called) {
+            $called = true;
+            $this->assertSame($message, $event->getMessage());
+        };
+        $this->dispatcher->addListener(StampieEvents::PRE_SEND, $listener);
 
         $this->delegate->expects($this->once())
             ->method('send')
-            ->with($this->equalTo($message))
-            ->will($this->returnValue(true));
+            ->with($this->identicalTo($message));
 
-        $this->dispatcher->expects($this->once())
-            ->method('dispatch')
-            ->with($this->equalTo(StampieEvents::PRE_SEND), $this->isInstanceOf('Stampie\Extra\Event\MessageEvent'));
+        $this->mailer->send($message);
+
+        $this->assertTrue($called, 'The listener should be called.');
+    }
+
+    public function testAllowsReplacingTheMessageInListeners()
+    {
+        $message = $this->getMockBuilder(MessageInterface::class)->getMock();
+        $newMessage = $this->getMockBuilder(MessageInterface::class)->getMock();
+
+        $this->dispatcher->addListener(StampieEvents::PRE_SEND, function (MessageEvent $event) use ($newMessage) {
+            $event->setMessage($newMessage);
+        });
+
+        $this->delegate->expects($this->once())
+            ->method('send')
+            ->with($this->identicalTo($newMessage));
 
         $this->mailer->send($message);
     }
